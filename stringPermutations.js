9e8107a386ee196bc7d34998db20373757d6bdb4
andrewkim <andrewk1216@gmail.com>
2020-01-07 02:50:50 -0500

    refactor and add comments

--- stringPermutations.js
+++ stringPermutations.js
@@ -1,30 +1,40 @@
-//given a string made up of unique characters, find and return all permutations of that string in an array
-//e.g. findPermutations('abcd') should return 
-// ['abcd', 'abdc', 'acbd', 'acdb', 'adcb', 'adbc', 'bacd', ...etc]
-// number of permutations is n!, where n is string length
-//can assume the string does not have duplicate characters for this problem
+//Given a string, finds and return all permutations of that string in an array
+//e.g. listPermutations('abc') will return ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+//maximum number of permutations (no duplicate characters) is n!, where n is string length
 
-const findPermutations = (string) => {
+const listPermutations = (string) => {
     let ret = [];
 
+    //function to create strings recursively, starting with n choices for the first character, n-1 for the second, etc.
     const recursiveFunc = (omittedString, remainingString) => {
         for (let i = 0; i < remainingString.length; i++) {
             let char = remainingString[i];
+
+            //remove the current iteration character from the input remaining string to be used as the new remaining string in the next depth call
             let str = remainingString.slice(0, i) + remainingString.slice(i + 1, remainingString.length);
+
+            //add the current iteration character to the input omitted string to be used as the new omitted string in the next depth call
             let newOmittedString = omittedString + char;
-            ret.push(newOmittedString);
+
+            //if recursion has reached maximum depth, push the constructed string to the array
+            if (newOmittedString.length === string.length) {
+                ret.push(newOmittedString);
+            }
             recursiveFunc(newOmittedString, str);
         }
     }
 
+    //call recursive function to build out the array
     recursiveFunc('', string);
 
-    console.log(ret);
-    ret = ret.filter((str) => {
-        return str.length === string.length;
+    //remove duplicate strings in case the input string has duplicate characters
+    ret = ret.filter((str, index, arr) => {
+        return (arr.indexOf(str) === index);
     })
 
     return ret;
 }
 
-console.log(findPermutations('abcd'));
+console.log(listPermutations('abcd'));
+console.log(listPermutations('aba'));
+console.log(listPermutations(''));
